fcf1d6667d3be7ef179e2fa8929d31d5a0ddf737
Francis Andrade <francis.andrade@trustly.com>
2024-01-30 14:16:57 -0300

    comment about local storage

--- app/hooks/useFavoriteSongs.ts
+++ app/hooks/useFavoriteSongs.ts
@@ -1,19 +1,10 @@
 import useFavoriteSongsStore from "@store/useFavoriteSongsStore";
-import { setToLocalStorage, getFromLocalStorage } from "@lib/localStorage";
-import { isEmptyArray } from "@lib/util";
-import { useEffect } from "react";
+import { setToLocalStorage } from "@lib/localStorage";
+
+//TODO: need to work on retrieving the data from the localStorage, since we may do this from a server component
 
 const useFavoriteSongs = () => {
   const { favoriteSongs, update } = useFavoriteSongsStore();
-
-  //TODO ver isso
-  // useEffect(() => {
-  //   const favoritesFromLocalStorage = getFromLocalStorage() as number[];
-  //   // console.log("favoritesFromLocalStorage", favoritesFromLocalStorage);
-  //   // if (!isEmptyArray(favoritesFromLocalStorage)) {
-  //   //   update([...favoritesFromLocalStorage]);
-  //   // }
-  // }, [update]);
 
   const isFavorite = (songId: number) => {
     return favoriteSongs.includes(songId);
